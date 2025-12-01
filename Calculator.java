2976ef4e74f51b5d3fc1e058c7ef04e8fd75bcf3
vinithreddybanda <vinithreddy08.123@gmail.com>
2025-12-02 02:25:23 +0530

    Initial commit: Java calculator

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+// Calculator.java
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+        System.out.print("Enter operator (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+        double result = 0;
+        switch (op) {
+            case '+': result = a + b; break;
+            case '-': result = a - b; break;
+            case '*': result = a * b; break;
+            case '/': result = a / b; break;
+            default: System.out.println("Invalid operator"); return;
+        }
+        System.out.println("Result: " + result);
+    }
+}
